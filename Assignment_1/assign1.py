664c20d96d04c201a701a901dc00f8e960530b03
kevin <kevin.dunphy1989@gmail.com>
2021-05-12 14:08:00 -0300

    Separate functions were a bit messy. All questions completed sequentially in  one script now, with helper functions for plotting. Need to improve logging and complete writeup, plots are in much better shape.

--- Assignment_1/assign1.py
+++ Assignment_1/assign1.py
@@ -12,18 +12,19 @@
 
 import os
 import time
+import logging
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import sounddevice as sd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from scipy.stats import probplot
+from scipy.stats import probplot, norm
 from scipy.signal import resample
 
 
 PLOT_DIR = os.sep.join([os.path.dirname(__file__), 'plots'])
-TEXTBOX_PROPS = {'boxstyle': 'round', 'facecolor': 'white', 'alpha': 0.5}
+TEXTBOX_PROPS = {'boxstyle': 'round', 'facecolor': 'white', 'alpha': 0.75}
 BIN_MODELS = {'fd': 'Freedman-Diaconis choice',
               'doane': 'Doane\'s formula',
               'scott': 'Scott\'s normal reference rule',
@@ -34,20 +35,6 @@ BIN_MODELS = {'fd': 'Freedman-Diaconis choice',
 
 sns.plotting_context("paper")
 sns.set_style("darkgrid")
-
-
-def set_minor_gridlines(ax):
-    """
-    Helper function to set minor gridlines on seaborn plots.
-    Args:
-        ax: input plot axis
-
-    Returns: None
-
-    """
-    ax.get_xaxis().set_minor_locator(mpl.ticker.AutoMinorLocator())
-    ax.get_yaxis().set_minor_locator(mpl.ticker.AutoMinorLocator())
-    ax.grid(b=True, which='minor', linewidth=0.5, linestyle=':')
 
 
 def assign1(data_file: str, headless: bool = True):
@@ -72,100 +59,171 @@ def assign1(data_file: str, headless: bool = True):
 
     """
     Question 1
-    a) Read in the data into a Pandas DataFrame
-    b) Generate a figure and plot of the timeseries data.
     """
-    timeseries_data = question_1a(data_file)
-    timeseries_figure, timeseries_plot = question_1b(timeseries_data,
-                                                     data_name,
-                                                     y_lim=(-3, 3),
-                                                     x_lim=(0, 0.5))
+    header_list = ['time', 'voltage']
+    # Read in the data
+    timeseries_data = pd.read_csv(data_file, usecols=[0, 1], names=header_list)
+
+    # Generate a figure and plot of the timeseries data.
+    timeseries_figure, timeseries_plot = plot_timeseries(timeseries_data['time'],
+                                                         timeseries_data['voltage'],
+                                                         title=f'Assignment 1, Question 1\nTimeseries of {data_name}',
+                                                         x_label='time (s)',
+                                                         y_label='voltage (V)',
+                                                         y_lim=(-3, 3),
+                                                         x_lim=(0, 0.5))
+
+    # Save the plot to the plots directory
+    timeseries_plot.figure.savefig(f'{PLOT_DIR}{os.sep}question_1_{data_name}.png')
 
     """
     Question 2
-    a) Calculate the sample rate, number of samples, and average (mean) voltage value.
-    b) Add those values to the timeseries plot generated in question 1
     """
-    sample_rate, num_samples, mean_voltage = question_2a(timeseries_data, data_name)
-    textbox = question_2b(timeseries_plot, sample_rate, num_samples, mean_voltage, data_name)
+    # Calculate the sample rate, number of samples, and average (mean) voltage value.
+    num_samples = len(timeseries_data)
+    sample_rate = (num_samples - 1) / (timeseries_data['time'].iloc[-1] - timeseries_data['time'].iloc[0])
+    mean_voltage = np.mean(timeseries_data['voltage'])
+
+    # Add those values to the timeseries plot generated in question 1
+    text_string = '\n'.join([r'$f_{s}=%.2f\ Hz$' % sample_rate,
+                             r'$N=%d$' % num_samples,
+                             r'$\mu=%.2f\ V$' % mean_voltage])
+    text_obj = timeseries_plot.text(0.05, 0.95, text_string,
+                                    transform=timeseries_plot.transAxes,
+                                    fontsize=14, verticalalignment='top', bbox=TEXTBOX_PROPS)
+    # Change title of plot to reflect question number
+    timeseries_plot.set(title=f'Assignment 1, Question 2\nTimeseries of {data_name}')
+
+    # Save to plots directory.
+    timeseries_plot.figure.savefig(f'{PLOT_DIR}{os.sep}question_2_{data_name}.png')
 
     """
     Question 3
-    a) Generate zero-mean time series and calculate variance and standard deviation.
-    b) Generate plot with standard deviation and variance.
     """
-    timeseries_data, standard_deviation, variance = question_3a(timeseries_data, mean_voltage)
+    # Generate zero-mean time series and calculate variance and standard deviation.
+    timeseries_data['zero mean voltage'] = timeseries_data['voltage'] - mean_voltage
+    standard_deviation = np.std(timeseries_data['zero mean voltage'])
+    variance = standard_deviation ** 2
 
-    # TODO: Generate plot and add text values.
+    # Generate timeseries plot.
+    zm_timeseries_figure, zm_timeseries_plot = plot_timeseries(timeseries_data['time'],
+                                                               timeseries_data['zero mean voltage'],
+                                                               title=f'Assignment 1, Question 3\nZero-mean timeseries of {data_name}',
+                                                               x_label='time (s)',
+                                                               y_label='voltage (V)',
+                                                               y_lim=(-3, 3),
+                                                               x_lim=(0, 0.5))
+
+    # Add text to plot.
+    text_string = '\n'.join([r'$\sigma=%.2f\ V$' % standard_deviation,
+                             r'$\sigma^2=%.2f\ V$' % variance])
+    text_obj = zm_timeseries_plot.text(0.05, 0.95, text_string,
+                                       transform=timeseries_plot.transAxes,
+                                       fontsize=14, verticalalignment='top', bbox=TEXTBOX_PROPS)
+
+    # Save the plot to the plots directory
+    zm_timeseries_plot.figure.savefig(f'{PLOT_DIR}{os.sep}question_3_{data_name}.png')
 
     """
-    # Question 4
-    # a) Calculate max amplitude and standard deviation ratio
-    # b) Append values to the zero-mean plot textbox.
+    Question 4
     """
-    max_amplitude, stddev_ratio = question_4a(timeseries_data, standard_deviation)
-    # TODO: add text to plot
+    # Calculate max amplitude and standard deviation ratio
+    max_amplitude = np.max(np.abs(timeseries_data['zero mean voltage']))
+    stddev_ratio = max_amplitude / standard_deviation
+
+    # Append values to the zero-mean plot textbox.
+    text_string += ('\n' + '\n'.join([r'$V_{max}=%.2f$' % max_amplitude,
+                                      r'$\frac{V_{max}}{\sigma}=%.2f$' %stddev_ratio]))
+    text_obj.set_text(text_string)
+
+    # Change title of plot to reflect question number
+    zm_timeseries_plot.set(title=f'Assignment 1, Question 4\nZero-mean timeseries of {data_name}')
+
+    # Save to plots directory.
+    zm_timeseries_plot.figure.savefig(f'{PLOT_DIR}{os.sep}question_4_{data_name}.png')
 
     """
     Question 5
-    Generate histogram of the data for various bin width models.
     """
-    # question_5(timeseries_data, data_name)
+    # Create a sub-directory to house all the histogram data
+    hist_plot_dir = f'{PLOT_DIR}{os.sep}histograms'
+    if not os.path.exists(hist_plot_dir):
+        os.makedirs(hist_plot_dir)
+
+    # common axis labels
+    x_label = 'zero-mean voltage(V)'
+    y_label = 'probability density function'
+
+    # Generate histogram for various bin width models:
+    for bin_model in BIN_MODELS:
+        bin_title = BIN_MODELS[bin_model]
+        # Create a unique title
+        title = f'Assignment 1, Question 5\nSample distribution of zero-mean {data_name} data\n{bin_title}'
+        # Generate histogram plot
+        fig, histogram_plot = plot_histogram(timeseries_data['zero mean voltage'],
+                                        x_label=x_label,
+                                        y_label=y_label,
+                                        title=title,
+                                        bin_model=bin_model)
+
+        # Save plot
+        histogram_plot.figure.savefig(f'{hist_plot_dir}{os.sep}question_5_{data_name}_{bin_model}.png')
 
     """
     Question 6
-    Generate normal probability plot
     """
-    question_6(timeseries_data, data_name)
-    # TODO: Comment on results
+    # Generate normal probability plot
+    fig, ax = normal_probability_plot(timeseries_data['zero mean voltage'])
+
+    # Update x-axis label and title.
+    plt.xlabel('zero-mean sample values (V)')
+    plt.title(f'Assignment 1, Question 6\nNormal probability plot for {data_name}')
+
+    # Save plot.
+    fig.savefig(f'{PLOT_DIR}{os.sep}question_6_probplot_{data_name}.png')
 
     """
     Question 7
-    a) Resample (upsample) signal to 44100 Hz
-    b) Play sound in speakers.
     """
-    normalized_data, sample_rate, duration = question_7a(timeseries_data)
-    # question_7b(normalized_data, sample_rate, duration)
+    # Resample (upsample) signal to 44100 Hz
+    sample_rate = 44100
+    duration = timeseries_data['time'].iloc[-1] - timeseries_data['time'].iloc[0]
+    resampled_data = resample(timeseries_data['zero mean voltage'], int(np.floor(duration * sample_rate)))
+
+    # Normalize to maximum amplitude of 1
+    normalized_data = resampled_data / np.max(resampled_data)
+
+    # Play sound in speakers.
+    sd.play(normalized_data, sample_rate)
+    time.sleep(duration)
+    sd.stop()
 
     if not headless:
         plt.show()
 
 
-def question_1a(data_file: str) -> pd.DataFrame:
-    """
-
-    Args:
-        data_file: path to timeseries .csv file.
-
-    Returns: The read in timeseries data in a pandas DataFrame.
-
-    """
-    header_list = ['time', 'voltage']
-    # Read in the data
-    timeseries_data = pd.read_csv(data_file, usecols=[0, 1], names=header_list)
-    return timeseries_data
-
-
-def question_1b(timeseries_data: pd.DataFrame,
-                data_name: str,
-                y_lim: tuple = None,
-                x_lim: tuple = None):
+def plot_timeseries(x: np.array,
+                    y: np.array,
+                    title: str = '',
+                    x_label: str = '',
+                    y_label: str = '',
+                    y_lim: tuple = None,
+                    x_lim: tuple = None):
     """
     Args:
-        timeseries_data:
-        data_name:
+        x:
+        y:
+        title:
+        x_label:
+        y_label:
         y_lim: If provided, plots will scale the y-axis to the bounds specified in a (bottom, upper) tuple.
         x_lim: If provided, plots will scale the x-axis to the bounds specified in a (bottom, upper) tuple.
 
     Returns:
 
     """
-    x_label = 'time (s)'
-    y_label = 'voltage (V)'
-    title = f'Assignment 1, Question 1\nTimeseries of {data_name}'
-
     fig = plt.figure()
-    line_plot = sns.lineplot(data=timeseries_data, x='time', y='voltage')
+    line_plot = sns.lineplot(x, y)
     line_plot.set(xlabel=x_label, ylabel=y_label, title=title)
     set_minor_gridlines(line_plot)
 
@@ -175,103 +233,50 @@ def question_1b(timeseries_data: pd.DataFrame,
     if x_lim is not None:
         plt.xlim(x_lim)
 
-    output_filename = PLOT_DIR + os.sep + f'question_1_{data_name}.png'
-    line_plot.figure.savefig(output_filename)
-
     return fig, line_plot
 
 
-def question_2a(timeseries_data: pd.DataFrame, data_name: str):
-    # TODO: Introduce logging
+def plot_histogram(data: np.array,
+                   x_label: str = '',
+                   y_label: str = '',
+                   title: str = '',
+                   bin_model: str = 'sqrt'):
 
-    num_samples = len(timeseries_data)
-    sample_rate = (num_samples - 1) / (timeseries_data['time'].iloc[-1] - timeseries_data['time'].iloc[0])
-    mean_voltage = np.mean(timeseries_data['voltage'])
-
-    return sample_rate, num_samples, mean_voltage
-
-
-def question_2b(timeseries_axis,
-                sample_rate,
-                num_samples,
-                mean_voltage,
-                data_name):
-
-    text_string = '\n'.join([r'$f_{s}=%.2f\ Hz$' % sample_rate,
-                         r'$N=%d$' % num_samples,
-                         r'$\mu=%.2f\ V$' % mean_voltage])
-    text_obj = timeseries_axis.text(0.05, 0.95, text_string,
-                                    transform=timeseries_axis.transAxes,
-                                    fontsize=14, verticalalignment='top', bbox=TEXTBOX_PROPS)
-    title = f'Assignment 1, Question 2\nTimeseries of {data_name}'
-    timeseries_axis.set(title=title)
-    output_filename = PLOT_DIR + os.sep + f'question_2_{data_name}.png'
-    timeseries_axis.figure.savefig(output_filename)
-    return text_obj
-
-
-def question_3a(timeseries_data: pd.DataFrame, mean_voltage):
-    timeseries_data['zero mean voltage'] = timeseries_data['voltage'] - mean_voltage
-    standard_deviation = np.std(timeseries_data['zero mean voltage'])
-    variance = standard_deviation ** 2
-
-    return timeseries_data, standard_deviation, variance
-
-
-def question_4a(timeseries_data, standard_deviation):
-    max_amplitude = np.max(np.abs(timeseries_data['zero mean voltage']))
-    stddev_ratio = max_amplitude / standard_deviation
-
-    return max_amplitude, stddev_ratio
-
-
-def question_5(timeseries_data: pd.DataFrame,
-               data_name: str,
-               bin_model: str = None):
-
-    hist_plot_dir = f'{PLOT_DIR}{os.sep}histograms'
-    if not os.path.exists(hist_plot_dir):
-        os.makedirs(hist_plot_dir)
-
-    x_label = 'zero-mean voltage bin (V)'
-    y_label = 'probability density function'
-
-    plt.figure()
+    fig = plt.figure()
     if bin_model is None:
-        bin_models = BIN_MODELS
-    else:
-        try:
-            bin_models = {bin_model: BIN_MODELS[bin_model]}
-        except KeyError:
-            raise ValueError("Invalid bin model name.")
+        bin_model = 'sqrt'
 
-    for bin_model, bin_title in bin_models.items():
-        plt.clf()
-        title = f'Assignment 1, Question 5\nSample distribution of zero-mean {data_name} data\n{bin_title}'
-        histogram_plot = sns.histplot(data=timeseries_data['zero mean voltage'], stat='density', bins=bin_model)
-        histogram_plot.set(xlabel=x_label, ylabel=y_label, title=title)
-        set_minor_gridlines(histogram_plot)
-        filename = f'{hist_plot_dir}{os.sep}question_5_{data_name}_{bin_model}.png'
-        histogram_plot.figure.savefig(filename)
+    histogram_plot = sns.histplot(data=data, stat='density', bins=bin_model)
+    histogram_plot.set(xlabel=x_label, ylabel=y_label, title=title)
+    set_minor_gridlines(histogram_plot)
+    return fig, histogram_plot
 
 
-def question_6(timeseries_data: pd.DataFrame, data_name: str):
+def normal_probability_plot(x):
+    (quantiles, values), (slope, intercept, r) = probplot(x, dist='norm')
     fig, ax = plt.subplots()
-    result = probplot(timeseries_data['zero mean voltage'], plot=ax)
+    ax.plot(values, quantiles, '+b')
+    ax.plot(quantiles * slope + intercept, quantiles, 'r')
+    percentiles = np.array([0.01, 0.02, 0.05, 0.10, 0.25, 0.5, 0.75, 0.90, 0.95, 0.98, 0.99])
+    ticks_quan = [norm.ppf(p) for p in percentiles]
+    plt.yticks(ticks_quan, percentiles)
+    plt.ylabel('probability')
+    plt.xlabel('data')
+    plt.ylim(norm.ppf(0.01), norm.ppf(0.99))
+    plt.xlim(-3, 3)
     set_minor_gridlines(ax)
-    filename = f'{PLOT_DIR}{os.sep}question_6_probplot_{data_name}.png'
-    fig.savefig(filename)
+    return fig, ax
 
 
-def question_7a(timeseries_data: pd.DataFrame):
-    sample_rate = 44100
-    duration = timeseries_data['time'].iloc[-1] - timeseries_data['time'].iloc[0]
-    resampled_data = resample(timeseries_data['zero mean voltage'], int(np.floor(duration * sample_rate)))
-    normalized_data = resampled_data / np.max(resampled_data)
-    return normalized_data, sample_rate, duration
+def set_minor_gridlines(ax):
+    """
+    Helper function to set minor gridlines on seaborn plots.
+    Args:
+        ax: input plot axis
 
+    Returns: None
 
-def question_7b(normalized_data, sample_rate, duration):
-    sd.play(normalized_data, sample_rate)
-    time.sleep(duration)
-    sd.stop()
+    """
+    ax.get_xaxis().set_minor_locator(mpl.ticker.AutoMinorLocator())
+    ax.get_yaxis().set_minor_locator(mpl.ticker.AutoMinorLocator())
+    ax.grid(b=True, which='minor', linewidth=0.5, linestyle=':')
