fb1a37d7fadf9b9aeed68ae4de2b12405aee1fc8
kevin <kevin.dunphy1989@gmail.com>
2021-05-18 17:11:37 -0300

    - Renaming to Assignment1a - Initial structure for Assignment1b

---
+++ Assignment1b/__init__.py
@@ -0,0 +1,11 @@
+"""
+    -*- coding: utf-8 -*-
+    Time    : 2021-05-18 5:05 p.m.
+    Author  : Kevin Dunphy
+    E-mail  : kevin.dunphy1989@gmail.com
+    FileName: __init__.py
+    
+    {Description}
+    -------------
+    
+"""
